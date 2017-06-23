4dd8c5385cdd16f3884e8c2b460fde4e3442085b
ScorchedGem <joshuacorney2@gmail.com>
2017-06-23 12:15:53 +1000

    Made a basic Network manager, still adding to it

---
+++ BLING-BLING-WRESTLING-RALLY/Assets/Scripts/StayHereScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayHereScript : MonoBehaviour
+{
+    public Transform obj;
+    public Vector3 pos;
+    public Quaternion rot;
+
+	// Use this for initialization
+	void Start ()
+    {
+        pos = obj.position;
+        rot = obj.rotation;        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        obj.position = pos;
+        obj.rotation = rot;	
+	}
+}
