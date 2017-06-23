6d5ca2bdbc75b9b4f1afccfa64f8812fa777043f
Nav <navin.brohier@gmail.com>
2017-06-23 14:21:55 +1000

    Some stuff
    + Mechanics script
    + Other things like textures

---
+++ BLING-BLING-WRESTLING-RALLY/Assets/Scripts/BattleScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleScript : MonoBehaviour {
+
+	public float Maxhealth, Strength;
+	private float Health, DecayTimer;
+	public float Decay, DecayRate;
+	public GameObject UI_Slider;
+
+	private bool Active;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Health = Maxhealth /2;
+		DecayTimer = DecayRate;
+		Active = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Health += Strength;
+		}
+		else
+		{
+			if (DecayTimer <= 0) Health = Health < Maxhealth / 2 ? Health += Decay * Time.deltaTime : Health -= Decay * Time.deltaTime;
+
+			DecayTimer = DecayTimer <= 0 ? DecayRate : DecayTimer -= Time.deltaTime;
+		}
+
+
+
+
+		UI_Slider.GetComponent<Slider>().value = Health;
+	}
+}
